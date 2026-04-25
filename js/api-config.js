c894e59fb4d8f88fb70115950701ef386ccd6df4
divadevaonline-cloud <divadevaonline@gmail.com>
2026-04-25 18:53:51 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = '';
+const GAS_API_URL = https://script.google.com/macros/s/AKfycbwlRdFOlMdX1hfBukqMf-tZeEJmTcwDwNR1Ovxd3AusaymIuqR4V7aRiU9iNZ5mqiz7/exec
