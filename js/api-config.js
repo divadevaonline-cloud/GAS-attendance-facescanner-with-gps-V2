da12282bc99c9c2feacbafd500baf76eac70dbd9
divadevaonline-cloud <divadevaonline-cloud@users.noreply.github.com>
2026-04-27 17:59:08 +0700

    Point API config to original GAS web app

--- js/api-config.js
+++ js/api-config.js
@@ -2,4 +2,4 @@
 //  ตั้งค่า Google Apps Script Web App URL
 //  แก้ไข URL ด้านล่างหากมีการ Deploy GAS ใหม่
 // ============================================================
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxWogc1LQbhl7e9brOJc6B9t_-x2GK21g_DLMYURT3tiyjqO_OaK3drAUs69i6U3f_h6g/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwlRdFOlMdX1hfBukqMf-tZeEJmTcwDwNR1Ovxd3AusaymIuqR4V7aRiU9iNZ5mqiz7/exec';
